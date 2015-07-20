e583be6479e3a4250b5fe50ea432e17ef82fc8a9
aid666 <aid666@gmail.com>
2015-07-21 01:10:55 +0800

    Update the evaluation creater

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'swagger-ui')));
 
+app.use(function(req, res, next) {
+
+  //to allow cross domain requests to send cookie information.
+  //res.header('Access-Control-Allow-Credentials', true);
+
+  // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
+  res.header('Access-Control-Allow-Origin',  '*');
+
+  // list of methods that are supported by the server
+  res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
+
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
+  next();
+});
 app.use('/evaluator/', evaluator);
 
 // catch 404 and forward to error handler
--- routes/evaluator.js
+++ routes/evaluator.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var Client = require('node-rest-client').Client
 var router = express.Router();
 
 /* GET evaluation listing. */
@@ -6,9 +7,46 @@ router.get('/:type/:catalog/:resKey', function(req, res, next) {
   res.json({});
 });
 
+
+function getProcessers(proType, catalog, resKey){
+  return [];
+}
+
+function locateEngineHub(){
+  return "http://localhost:12803/hub";
+}
+
+var evaluations = {};
+function saveEvaluation(catalog, resKey, procKey){
+  var dsKey = catalog + "/" + resKey;
+  if(evaluations[dsKey] == null){
+    evaluations[dsKey] = [];
+  }
+  evaluations[dsKey].push(procKey);
+  console.log("save evaluation: " + procKey)
+  return {
+    "key": procKey
+  };
+}
+
 /* POST evaluation to the resource. */
 router.post('/:type/:catalog/:resKey', function(req, res, next) {
-  res.json({});
+  var client = new Client();
+  var url = locateEngineHub() + "/processes";
+  var args = {
+    "processers": [],//getProcessers(req.params.type, req.params.catalog, req.params.resKey),
+    "data": req.body
+  };
+  console.log(url)
+  client.post(
+    url,
+    args,
+    function(data, response){
+      console.log(data.body);
+      var evaluation = saveEvaluation(req.params.catalog, req.params.resKey, data.procKey);
+      res.json(evaluation);
+    }
+  );
 });
 
 /* DELETE evaluation to the resource. */
